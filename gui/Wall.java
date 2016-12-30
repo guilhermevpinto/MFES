324aeb762aee5a36f9fec87fdf91635b32e0f645
guilhermevpinto@gmail.com <guilhermevpinto@gmail.com>
2016-12-30 01:45:20 +0000

    merged with master and gui folder exported

---
+++ gui/Wall.java
@@ -0,0 +1,57 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+
+@SuppressWarnings("serial")
+public class Wall extends JPanel {
+	
+	public static enum Orientation {HOR, VER};
+	
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private Orientation orientation;
+	private boolean set = false;
+	private boolean hover = false;
+	public Wall(int x, int y, Orientation or) {
+		this.x = x;
+		this.y = y;
+		this.orientation = or;
+		if (this.orientation == Orientation.HOR) {
+			this.width = QuaridorGui.WT * 4;
+			this.height = QuaridorGui.WT;
+		}
+		else if (this.orientation == Orientation.VER){
+			this.width = QuaridorGui.WT;
+			this.height = QuaridorGui.WT * 4;
+		}
+		
+		this.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(java.awt.event.MouseEvent evt) {
+				hover = true;
+				draw();
+            }
+			public void mouseExited(java.awt.event.MouseEvent evt) {
+				hover = false;
+				draw();
+            }
+		});
+	}
+
+	public void draw(){
+		repaint();
+		setBounds(x, y, width, height);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if(set | hover)
+			g.setColor(new Color(161,65,24,255));
+		else
+			g.setColor(new Color(250,150,120,100));
+		g.fillRect(0, 0, width, height);
+	}
+}
