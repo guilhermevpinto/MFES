1cf00917a81632c5eabef04cd2bb13b6462ea247
FilipaRamos <fimamora@hotmail.com>
2017-01-02 18:46:39 +0000

    corrected menu

--- gui/Background.java
+++ gui/Background.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
@@ -29,5 +30,11 @@ class Background extends JPanel {
 		}
 		
 	}
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
     
 }
